1803ad9be38f5edcc271e5d381322e9df289969c
Daniel Gibbs <me@danielgibbs.co.uk>
2019-11-13 10:18:27 +0000

    Update steamcmd_appid.sh

--- steamcmd_appid.sh
+++ steamcmd_appid.sh
@@ -10,7 +10,7 @@ curl https://api.steampowered.com/ISteamApps/GetAppList/v2/ | jq -r '.applist.ap
 echo "Creating steamcmd_appid.xml"
 curl https://api.steampowered.com/ISteamApps/GetAppList/v2/?format=xml > steamcmd_appid.xml
 echo "Creating steamcmd_appid.csv"
-cat steamcmd_appid.json | jq '.applist.apps[]' | jq -r '[.appid, .name] | @csv' > steamcmd_appid.csv
-cat steamcmd_appid.json | jq '.applist[]' | md-table > steamcmd_appid.md
+jq '.applist.apps[]' < steamcmd_appid.json | jq -r '[.appid, .name] | @csv' > steamcmd_appid.csv
+jq '.applist[]' < steamcmd_appid.json | md-table > steamcmd_appid.md
 echo "exit"
 exit
