d1bee5c29514e89a10f5c1fe7fbf66b9d1238ff9
Daniel Gibbs <me@danielgibbs.co.uk>
2020-05-24 16:02:48 +0100

    remove echo

--- steamcmd_appid.sh
+++ steamcmd_appid.sh
@@ -2,7 +2,7 @@
 # steamcmd_appid.sh
 # Author: Daniel Gibbs
 # Website: http://danielgibbs.co.uk
-# Version: 180826
+# Version: 200524
 # Description: Saves the complete list of all the appid their names in json and csv.
 
 echo "Creating steamcmd_appid.json"
@@ -12,5 +12,3 @@ curl https://api.steampowered.com/ISteamApps/GetAppList/v2/?format=xml > steamcm
 echo "Creating steamcmd_appid.csv"
 jq '.applist.apps[]' < steamcmd_appid.json | jq -r '[.appid, .name] | @csv' > steamcmd_appid.csv
 jq '.applist[]' < steamcmd_appid.json | md-table > steamcmd_appid.md
-echo "exit"
-exit
